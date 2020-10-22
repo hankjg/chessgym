8c43287e7fc1cca5b497fd314f33916cbaad7ae2
hankjg <henry.j.gregor@gmail.com>
2020-10-22 00:43:20 +0000

    Working checkboxes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,9 @@ class HomeController < ActionController::Base
 
     render({:template => "homeTemplates/settingsForm.html.erb"})
   end
+
+  def trainer
+    @input = params.fetch("P")
+    render({:template => "trainerTemplates/trainerPage.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  # ================== SETTINGS PAGES ==================
   get("/", { :controller => "home", :action => "settings" })
+  get("/settings", { :controller => "home", :action => "settings" })
+
+  # ================== TRAINING PAGES ==================
+  get("/trainer", { :controller => "home", :action => "trainer" })
 
 end
